a4f1fc5b113a2f83a79f87755af00c77bd2952aa
mdonit <91668983+mdonit@users.noreply.github.com>
2024-05-13 14:19:41 +0200

    feat: authentication (#3)
    Pages created: Home, Auth and 'Signup-successful' page
    Supabase Auth works: Sign up/in/out + email verification
    
    ---------
    
    Co-authored-by: marcigdd <79252919+marcigdd@users.noreply.github.com>
    Co-authored-by: Márk Vince Varga <varga.mrk@gmail.com>

---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import SignOutButton from '@/components/SignOutButton';
+import { User } from '@supabase/supabase-js';
+
+const Navbar = ({ user }: { user: User | null }) => {
+  return (
+    <>
+      <nav className="flex justify-between p-2 bg-slate-800">
+        <div className="flex items-center text-[2rem] text-white">DELFIN</div>
+        <div className="flex">
+          {user ? (
+            <>
+              <p className="flex items-center p-2 mr-10 text-white">
+                {user.email}
+              </p>
+              <SignOutButton />
+            </>
+          ) : (
+            <>
+              <Link
+                href="/auth-page?login=1"
+                className="flex items-center justify-center p-2 mr-10 bg-cyan-200 rounded-md"
+              >
+                Login
+              </Link>
+              <Link
+                href="/auth-page?login=0"
+                className="flex items-center justify-center p-2 bg-blue-600 rounded-md text-white"
+              >
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
